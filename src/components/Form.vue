3c22e4d7dbbc60c072a2de0c3b3f8c8cea603c5b
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2024-06-09 19:10:52 -0300

    feat: add responsiveness to main components

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,53 +1,75 @@
 <script setup lang="ts">
-    import { computed } from '@vue/reactivity';
-    import { ref } from 'vue';
+import { computed } from '@vue/reactivity';
+import { ref } from 'vue';
 
 
-    const number1 = ref(0);
-    const number2 = ref(0);
-    const operation = ref('add');
 
-    const getResult = ():number => {
-        let result = 0;
-        switch (operation.value) {
-            case 'add': result = number1.value + number2.value; break;
-            case 'subtract': result = number1.value - number2.value; break;
-            case 'multiply': result = number1.value * number2.value; break;
-            case 'divide': {
-                if (number1.value === 0 || number2.value === 0) {
-                    break;
-                }
+const number1 = ref(0);
+const number2 = ref(0);
+const operation = ref('add');
 
-                result = number1.value / number2.value; 
+const getResult = (): number => {
+    let result = 0;
+    switch (operation.value) {
+        case 'add': result = number1.value + number2.value; break;
+        case 'subtract': result = number1.value - number2.value; break;
+        case 'multiply': result = number1.value * number2.value; break;
+        case 'divide': {
+            if (number1.value === 0 || number2.value === 0) {
                 break;
             }
-            default: break;
+
+            result = number1.value / number2.value;
+            break;
         }
-         
-        return result;
+        default: break;
     }
 
-    const result = computed(() => getResult());
+    return result;
+}
+
+const result = computed(() => getResult());
 
 </script>
 
 
 <template>
-    <div class="row">
-        <div class="input-group mb-3 col">
-            <!-- <label class="form-label"  for="n-1">Número 1</label> -->
-            <input class="form-control" type="number" v-model="number1">
-            <span class="">
-                <select class="form-select" v-model="operation">
-                    <option value="add">Somar</option>
-                    <option value="subtract">Subtrair</option>
-                    <option value="multiply">Multiplicar</option>
-                    <option value="divide">Dividir</option>
-                </select>
-            </span>
-            <input class="form-control" type="number" v-model="number2">
-            <span class="input-group-text">=</span>
-            <input disabled class="form-control" id="n-1" type="number" :value="result">
+    
+    <div class="row gap-3 bg-info-subtle m-0 p-3 rounded">
+        <div class="col  p-0">
+            <div class="input-group ">
+                <input class="form-control" type="number" v-model="number1">
+                
+                <span class="input-group-text d-flex justify-content-center" style="width: 40px;"
+                    :class="{
+                        'bg-primary-subtle': operation === 'add',
+                        'bg-success-subtle': operation === 'subtract',
+                        'bg-warning-subtle': operation === 'multiply',
+                        'bg-danger-subtle': operation === 'divide'
+                        }"
+                >
+                    <i class="fa-solid " :class="{
+                        'fa-plus text-primary': operation === 'add',
+                        'fa-minus text-success': operation === 'subtract',
+                        'fa-xmark text-warning': operation === 'multiply',
+                        'fa-divide text-danger': operation === 'divide'                    
+                        }"></i>
+                </span>
+                
+                <input class="form-control" type="number" v-model="number2">
+                <span class="input-group-text bg-info-subtle">
+                    <i class="fa-solid fa-equals text-info"></i>
+                </span>
+                <input disabled class="form-control" id="n-1" type="number" :value="result">
+            </div>
+        </div>
+        <div class="col-md-auto p-0">
+            <select class="form-select" v-model="operation">
+                <option value="add">Somar</option>
+                <option value="subtract">Subtrair</option>
+                <option value="multiply">Multiplicar</option>
+                <option value="divide">Dividir</option>
+            </select>
         </div>
     </div>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="my-5 border rounded-2 p-3">
-        <h1>Calculadora Vue</h1>
+        <div class="d-flex align-items-center gap-3 mb-2">
+            <div class="px-1 rounded-3 bg-info-subtle text-info shadow">
+                <i class="bi bi-calculator fs-2"></i>
+            </div>
+            <h1 class="p-0 m-0">Calculadora Vue</h1>
+        </div>
         <small class="text-secondary">Simples calculadora aritimética construída com Vue.js</small>
     </div>
 </template>
